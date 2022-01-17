61f4fc2fcad06cc506375f9415472707ac5f65d7
c4r7u <nicolo.carluccio3@gmail.com>
2022-01-17 11:27:20 +0100

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <header>
+        <div class="margin-header">
+            <nav class="navbar">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img class="d-inline-block align-text-top" src="../assets/images/avadabarbers-logo-x1.png" alt="">
+                    </a>
+                    <form class="d-flex">
+                        <a class="form-control my-style" href="#"><i class="fas fa-shopping-cart"></i></a>
+                        <a class="btn" href="#"><i class="fas fa-bars"></i></a>
+                    </form>
+                </div>
+            </nav>
+        </div>
+    </header>
+</template>
+
+
+
+
+
+<style lang="scss" scoped>
+
+.margin-header {
+    margin: 0px 200px;
+}
+
+a img {
+    width: 100px;
+}
+
+.my-style {
+    border: 1px solid white;
+}
+
+.my-style:hover {
+    color: black;
+}
+</style>
